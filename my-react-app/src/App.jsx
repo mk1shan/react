dd4873bf3dd85939e03be8ac44561e07160df185
Mudipa Kishan <mudipakishanimayanga@gmail.com>
2026-03-13 14:25:54 +0530

    UPDATE

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,12 +4,16 @@ import UserGreeding from './UserGreeding.jsx';
 import List from './List.jsx';
 
 function App() {
-  return (
-  <>
-<List/>
-  </>
+ 
+ const fruits = [{id:1,name:"apple",calories:95},
+                {id:2,name:"orange",calories:45},
+                {id:3,name:"banana",calories:44}
+
+ ];
+
+ 
+  return (<List items={fruits} category=" Fruits"/>)
   
-  )
 }
 
-export default App;
+export default App
--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -1,12 +1,55 @@
 
 
-function List(){
+function List(props){
     
-    const fruits=['apple ',"orange ","banana ","cocnut ","pineapple"];
+const category = props.category;
+const itemList = props.itesm;
 
-    899880``
 
-    const listItems = fruits.map(fruits=><li>{fruits}</li>)
+const listItems = itemList.map(item=> <li key={item.id}>
+    {item.name}:&nbsp;
+    <b>{item.calories} </b>
+</li>)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     const fruits=
+//         [{id:1,name:'apple ',calories:45},
+//         {id:2,name:"orange",calories:56},
+//         {id:3,name:"banana",calories:32},
+//         {id:4,name:"cocnut",calories:45},
+//         {id:5,name:"pineapple",calories:120}];
+
+    
+// fruits.sort((a,b)=>a.name.localeCompare(b.name)); //alphanbetical
+
+// //const lowcalfruits =fruits.filter(fruit => fruit.calories <100);
+// const hgighcalfruits =fruits.filter(fruit => fruit.calories >100);
+
+
+//     const listItems = hgighcalfruits.map(hgighcalfruit=><li key={hgighcalfruit.id}>
+    
+//                                             {hgighcalfruit.name}:&nbsp;
+//                                             {hgighcalfruit.calories}</li>
+//     )
+
+
+
+
+
+
 
     return(<ol>{listItems}</ol>)
 }
